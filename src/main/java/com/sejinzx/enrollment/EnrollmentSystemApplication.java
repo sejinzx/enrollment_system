513a8b5301773d0a13614f5108dadce800017429
sejinzx <132969962+sejinzx@users.noreply.github.com>
2026-05-20 18:43:58 +0900

    chore: initial settings

--- src/main/java/com/sejinzx/enrollment/EnrollmentSystemApplication.java
+++ src/main/java/com/sejinzx/enrollment/EnrollmentSystemApplication.java
@@ -2,12 +2,14 @@ package com.sejinzx.enrollment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
-public class EnrollmentApplication {
+public class EnrollmentSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EnrollmentApplication.class, args);
+		SpringApplication.run(EnrollmentSystemApplication.class, args);
 	}
 
 }
