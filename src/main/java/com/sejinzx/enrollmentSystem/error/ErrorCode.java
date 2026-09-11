bd12eb24464f38fbf6e25b57afeb3a5961129628
sejinzx <132969962+sejinzx@users.noreply.github.com>
2026-09-11 22:58:24 +0900

    refactor: HTTP 상태 코드 처리를 HttpStatus 기반으로 변경

--- src/main/java/com/sejinzx/enrollmentSystem/error/ErrorCode.java
+++ src/main/java/com/sejinzx/enrollmentSystem/error/ErrorCode.java
@@ -4,25 +4,28 @@ import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
 
-    INVALID_INPUT(400, "잘못된 요청입니다"),
-    INVALID_PASSWORD(401, "비밀번호가 틀렸습니다"),
-    INVALID_REFRESH_TOKEN(401, "유효하지 않은 리프레시 토큰입니다."),
-    REFRESH_TOKEN_EXPIRED(401, "리프레시 토큰이 만료되었습니다."),
-    FORBIDDEN(403, "권한이 없습니다"),
-    CLASS_NOT_OPEN(403, "신청 가능한 상태가 아닙니다"),
-    CLASS_MODIFICATION_NOT_ALLOWED(403, "해당 상태에서는 수정할 수 없습니다"),
-    CLASS_DELETE_NOT_ALLOWED(403, "해당 상태에서는 삭제할 수 없습니다"),
-    USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다"),
-    CLASS_NOT_FOUND(404, "강의를 찾을 수 없습니다"),
-    ENROLL_NOT_FOUND(404, "수강 신청 내역을 찾을 수 없습니다"),
-    USERS_CLASS_NOT_FOUND(404, "사용자의 강의를 찾을 수 없습니다"),
-    DUPLICATE_USER_ID(409, "이미 존재하는 아이디입니다"),
-    DUPLICATE_ENROLL(409, "이미 수강신청을 했습니다"),
-    CLASS_CAPACITY_FULL(409, "정원이 초과되었습니다"),
-    CLASS_CURRENT_APPS_INVALID(409, "현재 신청 인원이 0명이라 감소할 수 없습니다."),
-    CANCEL_PERIOD_EXPIRED(409, "결제 후 3일이 지나 취소할 수 없습니다");
+    INVALID_INPUT(HttpStatus.BAD_REQUEST, "잘못된 요청입니다"),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 틀렸습니다"),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다."),
+    REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "리프레시 토큰이 만료되었습니다."),
 
-    private final int status;
+    FORBIDDEN(HttpStatus.FORBIDDEN, "권한이 없습니다"),
+    CLASS_NOT_OPEN(HttpStatus.FORBIDDEN, "신청 가능한 상태가 아닙니다"),
+    CLASS_MODIFICATION_NOT_ALLOWED(HttpStatus.FORBIDDEN, "해당 상태에서는 수정할 수 없습니다"),
+    CLASS_DELETE_NOT_ALLOWED(HttpStatus.FORBIDDEN, "해당 상태에서는 삭제할 수 없습니다"),
+
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다"),
+    CLASS_NOT_FOUND(HttpStatus.NOT_FOUND, "강의를 찾을 수 없습니다"),
+    ENROLL_NOT_FOUND(HttpStatus.NOT_FOUND, "수강 신청 내역을 찾을 수 없습니다"),
+    USERS_CLASS_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자의 강의를 찾을 수 없습니다"),
+
+    DUPLICATE_USER_ID(HttpStatus.CONFLICT, "이미 존재하는 아이디입니다"),
+    DUPLICATE_ENROLL(HttpStatus.CONFLICT, "이미 수강신청을 했습니다"),
+    CLASS_CAPACITY_FULL(HttpStatus.CONFLICT, "정원이 초과되었습니다"),
+    CLASS_CURRENT_APPS_INVALID(HttpStatus.CONFLICT, "현재 신청 인원이 0명이라 감소할 수 없습니다."),
+    CANCEL_PERIOD_EXPIRED(HttpStatus.CONFLICT, "결제 후 3일이 지나 취소할 수 없습니다");
+
+    private final HttpStatus status;
     private final String error;
 
     ErrorCode(HttpStatus status, String error) {
